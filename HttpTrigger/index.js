1254f8245181d4f5870e6afc0baeaf31ac7f9496
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2023-01-22 12:45:39 -0700

    added blobStorage

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,16 +1,18 @@
-const getEntries = require('../index');
 const axios = require('axios');
-const URL = 'https://terencewaters.com/aplusandminus/wp-json/wp/v2/posts?per_page=100';
 
+const getEntries = require('../index');
+const writeBlobStorage = require('blobStorage.js');
+const URL = 'https://terencewaters.com/aplusandminus/wp-json/wp/v2/posts?per_page=100';
 console.log('testing');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
     const blogData = getEntries();
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
+        status: 200, /* Defaults to 200 */
         blogData
     };
+
+    writeBlobStorage(context, req);
 }
